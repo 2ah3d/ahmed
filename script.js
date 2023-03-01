ac26cdbda4c4f74a3f5a1931f5b6179138e75c10
AHMED SALEM HUSSEIN <114582347+ahmed2salem@users.noreply.github.com>
2023-03-01 19:41:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$("#menu").click((function(){$(this).toggleClass("fa-times"),$("header").toggleClass("toggle")})),$(window).on("scroll load",(function(){$("#menu").removeClass("fa-times"),$("header").removeClass("toggle"),$(window).scrollTop()>0?$(".top").show():$("stop").hide()}))}));
